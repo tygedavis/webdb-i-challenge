363f19af832f5b1d246fdce010543d60a65d9ceb
tygedavis <tyge.davis@gmail.com>
2020-01-21 14:56:42 -0700

    POST request works

--- server.js
+++ server.js
@@ -6,4 +6,26 @@ const server = express();
 
 server.use(express.json());
 
+//GET -> General
+server.get('/', (req, res) => {
+  db('accounts')
+    .then(accounts => {
+      res.status(200).json(accounts);
+    })
+    .catch(err => {
+      res.status(500).json({ error: "There was an error retrieving the account data" });
+    });
+});
+
+//POST -> General
+server.post('/', (req,res) => {
+  db('accounts').insert(req.body)
+    .then(newAccount => {
+      res.status(201).json(newAccount);
+    })
+    .catch(err => {
+      res.status(500).json({ error: "There was an error posting the new account data" });
+    });
+});
+
 module.exports = server;
